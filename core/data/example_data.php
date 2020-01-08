c35ff8de14be1259ba840eeb63c0a8b13de6c594
Maqsat <sm@ilab.kz>
2020-01-08 12:10:39 +0600

    v1.1b

--- core/data/example_data.php
+++ core/data/example_data.php
@@ -1,4 +1,8 @@
 <?php
+    $tiles_block = "terra";
+    $tiles_css = "<link rel='stylesheet' href='blocks/$tiles_block/less/terra.css'>";
+
+
     $total_tiles = 5000;
     $world_x = 2000; //x = 2000
     $world_y = 3000; //y = 3000
--- core/scene_generator/index.php
+++ core/scene_generator/index.php
@@ -1,9 +1,12 @@
 <?php
-    include ('./core/data/example_data.php');
+    include ('../data/example_data.php');
+
+
+    echo $tiles_css;
+    include ('../../blocks/'.$tiles_block.'/index.html');
     //
     $scene_x = $_POST['scene_x'];
     $scene_y = $_POST['scene_y'];
     $scene_z = $_POST['scene_z'];
     //
-    echo "<h1>$scene_x:$scene_y:$scene_z</h1>";
 ?>
